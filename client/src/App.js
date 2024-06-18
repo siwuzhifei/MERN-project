0f99490aefdc728fef05e030a7de099453e2bd63
siwuzhifei <siwuzhifei@gmail.com>
2024-06-19 00:05:44 +1000

    client react to create folders likes source public and  components
    use react to create client front end.  create folders likes source, public and  components

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
